d6bf9afb04587ee2a128192d5c331819429a716f
RyabinovDmitrii <rojoc@yandex.ru>
2020-11-17 13:49:32 +0300

    изменения 1.1

--- Task1.4.1/src/main/java/jm/task/core/jdbc/Main.java
+++ Task1.4.1/src/main/java/jm/task/core/jdbc/Main.java
@@ -8,9 +8,7 @@ import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) throws SQLException {
 
-
         UserServiceImpl userService = new UserServiceImpl();
-
 
         userService.createUsersTable();
         userService.saveUser("Sasawwha", "adDzdaen", (byte) 11);
